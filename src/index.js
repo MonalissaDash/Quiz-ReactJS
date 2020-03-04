c9b6684a9d527aeeeae40ad7f2b9991e049471be
Dash <monalissa.dash.ext@nokia.com>
2020-03-04 16:13:47 +0530

    2nd Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService/index";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Quizee extends Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state={
+        questionBank:[],
+        score:0,
+        response:0
+    }
+    getQuestion=()=>{
+        quizService().then(question=>{
+            this.setState({
+                questionBank:question
+            });
+        });
+    };
+    ComputeAnswer=(answer,correct)=>{
+        if(answer===correct){
+            this.setState({
+                score:this.state.score+1
+            });
+        }
+        this.setState({
+            response:this.state.response < 5 ? this.state.response + 1 : 5
+        });
+    }
+    playAgain=()=>{
+        this.getQuestion();
+        this.setState({
+            score:0,
+            response:0
+        });
+    }
+
+    componentDidMount(){
+        this.getQuestion();
+    };
+
+
+    render(){
+        return(
+            <div className="container">
+                <div className="title">Quizee</div>
+               
+                {this.state.questionBank.length >0 && this.state.response<5 && this.state.questionBank.map(({question,answers,correct,questionId})=>
+                 (<QuestionBox question={question} options={answers} key={questionId} 
+                 selected={answer=>(this.ComputeAnswer(answer,correct))}/>))}
+                 {this.state.response===5?(<Result score={this.state.score} playAgain={this.playAgain}/>):null}
+                
+            </div>
+            
+        )};
+
+}
+ReactDOM.render(<Quizee/>,document.getElementById("root"));
+
+
